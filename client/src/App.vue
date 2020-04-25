ef01f1166f3d96d710e349a8fac38263e387e410
贾顺名 <jiashunming@outlook.com>
2020-04-25 23:03:40 +0800

    build: first commit

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <Login title="问卷调查系统后台登录"/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+.logo {
+  width: 200px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <div class="login-panel">
+      <el-row class="input-row">
+        <el-col :span="8">
+          <div class="input-field">账号：</div>
+        </el-col>
+        <el-col :span="16">
+          <div class="input-field">
+            <el-input
+              placeholder="请输入账号"
+              v-model="username"
+            >
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row class="input-row">
+        <el-col :span="8">
+          <div class="input-field">密码：</div>
+        </el-col>
+        <el-col :span="16">
+          <div class="input-field">
+            <el-input
+              placeholder="请输入密码"
+              show-password
+              v-model="password"
+            >
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row class="button-row">
+        <el-col :span="8" :offset="16">
+          <el-button type="primary" round>登录</el-button>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.container {
+  width: 480px;
+  margin: 0 auto;
+}
+.input-row {
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+}
+.input-field {
+  text-align: center;
+}
+.button-row {
+  text-align: right;
+}
+</style>
