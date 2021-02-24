eda63ef850754ab3b1020a7cd05a5bba49bed4f1
jarvis <jiashunming@outlook.com>
2021-02-24 20:34:58 +0800

    build: upload dist

--- db/init.sql
+++ db/init.sql
@@ -4,29 +4,29 @@ USE `project_management`;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` INT(11) AUTO_INCREMENT,
+  `id` INT(10) AUTO_INCREMENT,
   `username` VARCHAR(20) NOT NULL,
   `password` VARCHAR(20) NOT NULL,
   `role_id` INT(2) NOT NULL,
   `status` INT(2) DEFAULT 0,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS  `user_role`;
 CREATE TABLE `user_role` (
-  `id` INT(11) AUTO_INCREMENT,
+  `id` INT(2) AUTO_INCREMENT,
   `text` TEXT NOT NULL,
   `desc` TEXT,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS `group`;
 CREATE TABLE `group` (
-  `id` INT(11) AUTO_INCREMENT,
+  `id` INT(10) AUTO_INCREMENT,
   `name` VARCHAR(20) NOT NULL,
   `status` INT(2) NOT NULL,
   `creator` INT(10) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS `group_member`;
@@ -35,17 +35,17 @@ CREATE TABLE `group_member` (
   `uid` INT(10) NOT NULL,
   `role_id` INT(2) NOT NULL,
   `status` INT(2) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`group_id`, `uid`)
 );
 
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
-  `id` INT(11) AUTO_INCREMENT,
+  `id` INT(10) AUTO_INCREMENT,
   `creator` INT(10) NOT NULL,
   `group_id` INT(10) NOT NULL,
   `name` VARCHAR(20) NOT NULL,
   `status` INT(2) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS `project_member`;
@@ -54,46 +54,74 @@ CREATE TABLE `project_member` (
   `uid` INT(10) NOT NULL,
   `role_id` INT(2) NOT NULL,
   `status` INT(2) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`project_id`, `uid`)
 );
 
 DROP TABLE IF EXISTS  `member_role`;
 CREATE TABLE `member_role` (
-  `id` INT(11) AUTO_INCREMENT,
+  `id` INT(2) AUTO_INCREMENT,
   `text` TEXT NOT NULL,
   `desc` TEXT,
-  PRIMARY KEY(id)
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS `task_group`;
 CREATE TABLE `task_group` (
-  id INT(10) AUTO_INCREMENT,
-  title VARCHAR(20) NOT NULL,
-  desc VARCHAR(20) NOT NULL,
-  creator INT(10) NOT NULL,
-  status INT(2) NOT NULL,
-  PRIMARY KEY(id)
+  `id` INT(10) AUTO_INCREMENT,
+  `title` VARCHAR(20) NOT NULL,
+  `desc` VARCHAR(20) NOT NULL,
+  `creator` INT(10) NOT NULL,
+  `status` INT(2) NOT NULL,
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
-  id INT(10) AUTO_INCREMENT,
-  title VARCHAR(20) NOT NULL,
-  desc VARCHAR(20) NOT NULL,
-  creator INT(10) NOT NULL,
-  status INT(2) NOT NULL,
-  expire_date INT(13) NOT NULL,
-  task_group_id INT(10),
-  PRIMARY KEY(id)
+  `id` INT(10) AUTO_INCREMENT,
+  `title` VARCHAR(20) NOT NULL,
+  `desc` VARCHAR(20) NOT NULL,
+  `creator` INT(10) NOT NULL,
+  `status` INT(2) NOT NULL,
+  `expire_date` INT(13) NOT NULL,
+  `task_group_id` INT(10),
+  `parent_task_id` INT(10),
+  `type` INT(2) NOT NULL,
+  `priority` INT(2) NOT NULL,
+  PRIMARY KEY(`id`)
 );
 
 DROP TABLE IF EXISTS `task_comment`;
 CREATE TABLE `task_comment` (
-  id INT(10) AUTO_INCREMENT,
-  task_id INT(10) NOT NULL,
-  creator INT(10) NOT NULL,
-  text TEXT NOT NULL,
-  status INT(2) NOT NULL,
-  datetime INT(13) NOT NULL,
-  PRIMARY KEY(id)
+  `id` INT(10) AUTO_INCREMENT,
+  `task_id` INT(10) NOT NULL,
+  `creator` INT(10) NOT NULL,
+  `text` TEXT NOT NULL,
+  `status` INT(2) NOT NULL,
+  `datetime` INT(13) NOT NULL,
+  PRIMARY KEY(`id`)
+);
+
+DROP TABLE IF EXISTS  `task_type`;
+CREATE TABLE `task_type` (
+  `id` INT(2) AUTO_INCREMENT,
+  `text` TEXT NOT NULL,
+  `desc` TEXT,
+  PRIMARY KEY(`id`)
+);
+
+DROP TABLE IF EXISTS  `priority_type`;
+CREATE TABLE `priority_type` (
+  `id` INT(2) AUTO_INCREMENT,
+  `text` TEXT NOT NULL,
+  `desc` TEXT,
+  PRIMARY KEY(`id`)
+);
+
+
+DROP TABLE IF EXISTS  `task_participant`;
+CREATE TABLE `task_participant` (
+  `task_id` INT(10) NOT NULL,
+  `uid` INT(10) NOT NULL,
+  `add_date` INT(13) NOT NULL,
+  PRIMARY KEY(`task_id`, `uid`)
 );
