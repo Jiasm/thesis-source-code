e7e9c32cb8aad707a3de046b553fb4d1d9a39906
jarvis <jiashunming@outlook.com>
2021-02-27 17:58:53 +0800

    build: update sql

--- db/init.sql
+++ db/init.sql
@@ -143,3 +143,15 @@ VALUES (1, 1, 1, 1), (2, 2, 2, 1);
 
 INSERT INTO `project` (`creator`, `group_id`, `name`, `status`)
 VALUES (3, 1, '测试项目 1', 1), (4, 2, '测试项目 2', 1);
+
+INSERT INTO `priority_type` (`text`)
+VALUES ('紧急', '重要', '普通');
+
+INSERT INTO `task_type` (`text`)
+VALUES ('需求', '缺陷');
+
+INSERT INTO `task` (`title`, `desc`, `creator`, `status`, `expire_date`, `type`, `priority`)
+VALUES ('测试任务 1', '任务描述', 3, 1, 1614419750, 1, 1);
+
+INSERT INTO `task_participant` (`task_id`, `uid`, `add_date`)
+VALUES (1, 2, 1614419750);
