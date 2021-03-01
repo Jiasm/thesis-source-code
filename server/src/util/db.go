b9e60e1d1effa1bb583c8b9c522b005569a4eaac
jarvis <jiashunming@outlook.com>
2021-03-01 21:14:12 +0800

    feat: 更新 DB 配置读取为动态文件读取

--- server/src/util/db.go
+++ server/src/util/db.go
@@ -2,10 +2,23 @@ package util
 
 import (
 	"database/sql"
-	"strings"
-	"log"
+	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
+
+var configPath string = os.Getenv("CONFIG_PATH")
+
+type Config struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	IP string `json:"ip"`
+	Port string `json:"port"`
+	DBName string `json:"dbname"`
+}
 
 //数据库的配置
 const (
@@ -21,9 +34,18 @@ const (
 var DB *sql.DB
 
 func InitDB()  {
+	buf, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Panicln("load config conf failed: ", err)
+	}
+	config := &Config{}
+	err = json.Unmarshal(buf, config)
+	if err != nil {
+		log.Panicln("decode config file failed:", string(buf), err)
+	}
 	//构建连接："用户名:密码@tcp(IP:端口)/数据库?charset=uft8"
 	//注意：要想解析time.Time类型，必须要设置parseTime=True
-	path := strings.Join([]string{username,":",password,"@tcp(",ip,":",port,")/",dbName,"?charset=utf8&parseTime=True&loc=Local"},"")
+	path := strings.Join([]string{ config.Username, ":", config.Password, "@tcp(", config.IP, ":", config.Port, ")/", config.DBName, "?charset=utf8&parseTime=True&loc=Local" }, "")
 	//打开数据库，前者是驱动名，所以要导入:_"github.com/go-sql-driver/mysql"
 	DB,_ = sql.Open(driverName,path)
 	//设置数据库最大连接数
